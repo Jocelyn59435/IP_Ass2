56805aa2e87718cca3414ad970dfe2c8a786383f
unknown <jocelynhuang193@outlook.com>
2021-05-20 22:27:16 +1000

    update server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const session = require('express-session');
 const app = express();
-// const port = 5000;
 const port = process.env.PORT || 5000;
 app.use(express.static('public'));
 
